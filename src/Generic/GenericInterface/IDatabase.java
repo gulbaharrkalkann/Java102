7da4e5d24f44eb15037ca08e96151827a5e0d819
gulbaharrkalkann <gulbaharrkalkan@gmail.com>
2025-07-11 12:58:29 +0300

    generic sınıflar eklendi

--- src/Generic/GenericInterface/IDatabase.java
+++ src/Generic/GenericInterface/IDatabase.java
@@ -1,5 +1,12 @@
 package Generic.GenericInterface;
 
-public interface IDatabase {
+public interface IDatabase <T> {
 
+    public boolean add(T data);
+
+    public boolean delete(T data);
+
+    public boolean update(T data);
+
+    public T select();
 }
--- src/Generic/GenericInterface/Main.java
+++ src/Generic/GenericInterface/Main.java
@@ -3,5 +3,7 @@ package Generic.GenericInterface;
 public class Main {
     public static void main(String[] args) {
 
+        Student <String> stu =new Student<>();
+        stu.add("ABC");
     }
 }
